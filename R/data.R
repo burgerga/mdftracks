d08552483dc3b800a897f723b04be8b28a7f2833
Gerhard Burger <burger.ga@gmail.com>
2017-02-06 12:00:41 +0100

    Add example data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Example data to show mdftracks functionality
+#'
+#' @format A data frame with 10 rows and 9 variables
+#' \describe{
+#'   \item{cl}{track cluster}
+#'   \item{id}{track identifier (in cluster)}
+#'   \item{p}{point in track (not necessarily the same as frame number)}
+#'   \item{x}{x-coordinate of point}
+#'   \item{y}{y-coordinate of point}
+#'   \item{z}{z-coordinate of point}
+#'   \item{t}{time of point}
+#'   \item{ch}{track channel}
+#'   \item{uid}{track identifier (unique over clusters)}
+#' }
+#' @source Self-generated
+"mdftracks.example.data"
